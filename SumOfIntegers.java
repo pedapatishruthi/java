f91fae1af67219b9d2c4bc248d108054cb16bb73
pedapatishruthi <143867700+pedapatishruthi@users.noreply.github.com>
2023-09-09 00:05:37 -0700

    Create SumOfIntegers.java

---
+++ SumOfIntegers.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class SumOfIntegers extends Object{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the two integers for addition");
+        int x= sc.nextInt();
+        int y= sc.nextInt();
+        int z= x+y;
+        System.out.println("Sum of "+x+" and "+y+" is "+z);
+    }
+}
